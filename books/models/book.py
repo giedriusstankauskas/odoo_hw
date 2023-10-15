7d9296f7ff5aee497498ca1902d13b8732262c7a
giedriusstankauskas <46478940+giedriusstankauskas@users.noreply.github.com>
2023-10-16 00:59:36 +0300

    clean code

--- books/models/book.py
+++ books/models/book.py
@@ -8,14 +8,3 @@ class my_module(models.Model):
     name = fields.Char(string="Pavadinimas")
     description = fields.Char(string="Aprašymas")
     company = fields.Many2one('res.company', string="Įmonė")
-
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
